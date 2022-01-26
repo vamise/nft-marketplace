fc44a3e594052dd602722e4b1946cec21cac741f
Miko <miko@fyfy.io>
2022-01-26 14:13:37 +0000

    Fix

--- js/packages/web/src/hooks/useCreator.ts
+++ js/packages/web/src/hooks/useCreator.ts
@@ -1,44 +1,11 @@
-import { useMemo } from 'react';
+import { StringPublicKey, pubkeyToString } from '@oyster/common';
 import { useMeta } from '../contexts';
-import { Artist } from '../types';
-import { AuctionView } from './useAuctions';
 
-export const useCreators = (auction?: AuctionView) => {
+export const useCreator = (id?: StringPublicKey) => {
   const { whitelistedCreatorsByCreator } = useMeta();
-
-  const creators = useMemo(
-    () =>
-      [
-        ...(
-          [
-            ...(auction?.items || []).flat().map(item => item?.metadata),
-            auction?.thumbnail?.metadata,
-          ]
-            .filter(item => item && item.info)
-            .map(item => item?.info.data.creators || [])
-            .flat() || []
-        )
-          .filter(creator => creator.verified)
-          .reduce((agg, item) => {
-            agg.set(item.address, item.share);
-            return agg;
-          }, new Map<string, number>())
-          .entries(),
-      ].map(creatorArray => {
-        const [creator, share] = creatorArray;
-        const knownCreator = whitelistedCreatorsByCreator[creator];
-
-        return {
-          address: creator,
-          verified: true,
-          share: share,
-          image: knownCreator?.info.image || '',
-          name: knownCreator?.info.name || '',
-          link: knownCreator?.info.twitter || '',
-        } as Artist;
-      }),
-    [auction, whitelistedCreatorsByCreator],
+  const key = pubkeyToString(id);
+  const creator = Object.values(whitelistedCreatorsByCreator).find(
+    creator => creator.info.address === key,
   );
-
-  return creators;
+  return creator;
 };
